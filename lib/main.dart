f409c44f2c879642c1227f1274bd44caeb6602d9
Victor Afonso <71721377+VictorAfonsofrwk@users.noreply.github.com>
2021-12-24 11:28:41 -0300

    Feature/SignUp (#23)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fidelidade_android/pages/SignUp/SignUpPage.dart';
 import 'package:fidelidade_android/pages/HomePage.dart';
 import 'package:fidelidade_android/pages/Welcome/WelcomeScreen.dart';
 import 'package:flutter/material.dart';
@@ -10,12 +11,14 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       title: "+Fideliade",
       initialRoute: '/',
       theme: ThemeData(primarySwatch: Colors.blue),
       routes: {
         '/': (context) => WelcomeScreen(),
         '/home': (context) => const HomePage(),
+        '/cadastro': (context) => const SignUpPage(),
       },
       debugShowCheckedModeBanner: false,
     );
