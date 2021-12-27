3c2822ad818ef734021a5da0e45ed1aa71cd9d4c
AdrianoValadaresFrwk <adrianovaladares@frwk.com.br>
2021-12-27 19:20:15 -0300

    Implementação do grafico

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       title: "+Fideliade",
       initialRoute: '/',
       theme: ThemeData(primarySwatch: Colors.blue),
