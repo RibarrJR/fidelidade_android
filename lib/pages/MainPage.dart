3fd36d3bfa4f9bddecf423b332b521122e1952a6
AdrianoValadaresFrwk <adrianovaladares@frwk.com.br>
2021-12-28 11:04:35 -0300

    Teste de animações

--- lib/pages/MainPage.dart
+++ lib/pages/MainPage.dart
@@ -12,22 +12,53 @@ class MainPage extends StatefulWidget {
   }
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
   int _index = 0;
-  final screens = [const HomePage(), const MenuPage()];
+  final screens = const <Widget>[
+    HomePage(
+      key: PageStorageKey<String>('pageOne'),
+    ),
+    MenuPage(
+      key: PageStorageKey<String>('pageTwo'),
+    ),
+  ];
+  final PageStorageBucket _bucket = PageStorageBucket();
+  late TabController _tabController;
 
   void onNavChanges(int index) {
     setState(() {
       _index = index;
+      _tabController.animateTo(_index, duration: const Duration(seconds: 1));
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: _index,
-        children: screens,
+      // body: IndexedStack(
+      //   index: _index,
+      //   children: screens,
+      // ),
+      // body: TabBarView(
+      //   controller: _tabController,
+      //   children: const <Widget>[
+      //     HomePage(),
+      //     MenuPage(),
+      //   ],
+      // ),
+      body: PageStorage(
+        bucket: _bucket,
+        child: TabBarView(
+          key: const PageStorageKey<String>('tab'),
+          controller: _tabController,
+          children: screens,
+        ),
       ),
       bottomNavigationBar: AppNavBar(
         onNavChange: onNavChanges,
