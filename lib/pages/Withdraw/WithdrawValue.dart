b0872917fa709a88d8b90a484c3c478d5ce02046
ImGuilhermeXavier <gtech.xavier@gmail.com>
2021-12-29 11:57:52 -0300

    Adicionar alterações da pasta

--- lib/pages/Withdraw/WithdrawValue.dart
+++ lib/pages/Withdraw/WithdrawValue.dart
@@ -1,8 +1,9 @@
 import 'package:fidelidade_android/components/CustomAppBar.dart';
+import 'package:fidelidade_android/components/Input.dart';
 import 'package:flutter/material.dart';
 
 class WithdrawValue extends StatelessWidget {
-  const WithdrawValue({Key? key}) : super(key: key);
+  final myController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +11,27 @@ class WithdrawValue extends StatelessWidget {
       appBar: CustomAppBar(
         title: "Sacar - Pix",
       ),
-      body: null,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const Text(
+            'Quanto gostaria de sacar',
+            textAlign: TextAlign.center,
+          ),
+          Container(
+            child: Column(
+                children: const [Text('Saldo total'), Text('R\$ 3.200.00')]),
+          ),
+          Column(
+            children: [
+              const Text('Valor'),
+              // Input(
+              //     controller: myController,
+              //     labelText: 'Entre com o valor desejado')
+            ],
+          )
+        ],
+      ),
     );
   }
 }
