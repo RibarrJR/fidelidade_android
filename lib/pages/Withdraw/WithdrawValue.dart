98bab589daf82e23a07dab34f674a16f67989ae0
Guilherme Caçador <gtech.xavier@gmail.com>
2022-01-03 16:38:22 -0300

    Incluir espaçamentos necessarios

--- lib/pages/Withdraw/WithdrawValue.dart
+++ lib/pages/Withdraw/WithdrawValue.dart
@@ -2,6 +2,7 @@ import 'package:fidelidade_android/components/CustomAppBar.dart';
 import 'package:fidelidade_android/components/Input.dart';
 import 'package:fidelidade_android/components/RoundedButton.dart';
 import 'package:fidelidade_android/components/RoundedInputField.dart';
+import 'package:fidelidade_android/constants.dart';
 import 'package:flutter/material.dart';
 
 class WithdrawValue extends StatelessWidget {
@@ -9,31 +10,70 @@ class WithdrawValue extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: CustomAppBar(
         title: "Sacar - Pix",
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          const Text(
-            'Quanto gostaria de sacar?',
-            textAlign: TextAlign.center,
-          ),
-          Container(
-            child: Column(
-                children: const [Text('Saldo total'), Text('R\$ 3.200.00')]),
-          ),
-          RoundedInputField(
-            labelText: 'Valor',
-            hintText: 'Entre com o valor desejado',
-            onChanged: (value) {},
-          ),
-          RoundedButton(
-            text: "Sacar",
-            press: () {},
-          ),
-        ],
+      body: Container(
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                'Quanto gostaria de sacar?',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 40,
+                    letterSpacing: 4,
+                    fontWeight: FontWeight.bold,
+                    color: kPrimaryColor),
+              ),
+            ),
+            Container(
+              width: size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 60),
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              color: primaryColor,
+              child: Column(children: const [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Saldo total',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 2),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 30),
+                  child: Text(
+                    'R\$ 3.200.00',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        letterSpacing: 4,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ]),
+            ),
+            RoundedInputField(
+              labelText: 'Valor',
+              hintText: 'Entre com o valor desejado',
+              onChanged: (value) {},
+            ),
+            RoundedButton(
+              text: "Sacar",
+              press: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
