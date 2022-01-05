baf76c5f31bf27d9908458b21c7d7dd8485b4ff9
Victor Afonso <71721377+VictorAfonsofrwk@users.noreply.github.com>
2022-01-05 09:50:19 -0300

    Feature/extract (#66)
    * feature/extractpage
    
    * feature/uncoupleExtractCard
    
    * fix/excluding-comments

--- lib/features/Extract/presentation/pages/ExtractPage.dart
+++ lib/features/Extract/presentation/pages/ExtractPage.dart
@@ -1,4 +1,6 @@
+import 'package:fidelidade_android/features/Extract/presentation/widgets/ExtractCardWidget.dart';
 import 'package:fidelidade_android/shared/presentation/widgets/CustomAppBar.dart';
+import 'package:fidelidade_android/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class ExtractPage extends StatefulWidget {
@@ -18,11 +20,25 @@ class _ExtractPageState extends State<ExtractPage> {
       ),
       body: ListView(
         children: [
-          Container(
-            height: 20,
+          ExtractCard(
+            date: '01/01/2022',
+            amount: 'R\$ 5,00',
+            type: CardType.WITHDRAW,
+          ),
+          ExtractCard(
+            date: '01/01/2022',
+            amount: 'R\$ 5,00',
+            type: CardType.TRANSFER,
+          ),
+          ExtractCard(
+            date: '01/01/2022',
+            amount: '500',
+            type: CardType.RECEIVE,
           ),
         ],
       ),
     );
   }
 }
+
+enum CardType { TRANSFER, WITHDRAW, RECEIVE }
