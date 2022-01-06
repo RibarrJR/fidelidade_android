6ad9c45baa7f01f370b485bd173e9020f0bcc6a8
AdrianoValadaresFrwk <adrianovaladares@frwk.com.br>
2022-01-06 14:49:41 -0300

    Implementação das animações da nav bar

--- lib/shared/presentation/widgets/AppNavBar.dart
+++ lib/shared/presentation/widgets/AppNavBar.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:fidelidade_android/utils/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +14,17 @@ class AppNavBar extends StatefulWidget {
   }
 }
 
-class _AppNavBarState extends State<AppNavBar> {
+class _AppNavBarState extends State<AppNavBar> with TickerProviderStateMixin {
+  late Animation<double> archHeightAnimation;
+  late AnimationController archHeightController;
+  late AnimationController tabSliderController;
+  late Map<String, Animation<double>?> tabSliderAnimations = {
+    "a1": null,
+    "a2": null,
+    "a3": null,
+    "a4": null,
+  };
+
   int index = 0;
   late Function _onNavChange;
 
@@ -20,6 +32,78 @@ class _AppNavBarState extends State<AppNavBar> {
   void initState() {
     _onNavChange = widget.onNavChange;
     super.initState();
+
+    tabSliderController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 100),
+    );
+
+    Map<String, Tween<double>> valuesT = {
+      "v1": Tween(begin: 0.10, end: 0.61),
+      "v2": Tween(begin: 0.15, end: 0.66),
+      "v3": Tween(begin: 0.35, end: 0.86),
+      "v4": Tween(begin: 0.40, end: 0.91),
+    };
+
+    tabSliderAnimations = {
+      'a1': valuesT["v1"]?.animate(tabSliderController)
+        ?..addListener(() {
+          setState(() {});
+        })
+        ..addStatusListener((status) {
+          if (status == AnimationStatus.completed ||
+              status == AnimationStatus.dismissed) {
+            archHeightController.forward(from: 0.0);
+          }
+        }),
+      'a2': valuesT["v2"]?.animate(tabSliderController)
+        ?..addListener(() {
+          setState(() {});
+        }),
+      'a3': valuesT["v3"]?.animate(tabSliderController)
+        ?..addListener(() {
+          setState(() {});
+        }),
+      'a4': valuesT["v4"]?.animate(tabSliderController)
+        ?..addListener(() {
+          setState(() {});
+        }),
+    };
+
+    archHeightController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 100),
+    );
+
+    Tween<double> _archHeight = Tween(begin: 0.0, end: 20);
+    AnimationStatus prevStatus = AnimationStatus.dismissed;
+    int direction = 1;
+
+    archHeightAnimation = _archHeight.animate(archHeightController)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (prevStatus == AnimationStatus.reverse &&
+            status == AnimationStatus.dismissed) {
+          if (direction == 1) {
+            tabSliderController.forward(from: 0.0);
+            direction = 0;
+          } else {
+            tabSliderController.reverse(from: 1.0);
+            direction = 1;
+          }
+        }
+        prevStatus = status;
+      });
+    archHeightController.forward(from: 0.0);
+  }
+
+  @override
+  void dispose() {
+    tabSliderController.dispose();
+    archHeightController.dispose();
+    super.dispose();
   }
 
   @override
@@ -33,67 +117,68 @@ class _AppNavBarState extends State<AppNavBar> {
         children: [
           CustomPaint(
             size: Size(_size.width, _size.height),
-            painter: NavBarCustomPainter(index),
+            painter: NavBarCustomPainter(
+                index: index,
+                archRadius: archHeightAnimation.value,
+                tabPositions: tabSliderAnimations),
           ),
-          SizedBox(
-            width: _size.width,
-            height: 80,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  mainAxisAlignment: index == 1
-                      ? MainAxisAlignment.center
-                      : MainAxisAlignment.start,
-                  children: [
-                    IconButton(
-                        color: Colors.white,
-                        onPressed: () {
-                          setState(() {
-                            index = 0;
-                            _onNavChange(index);
-                          });
-                        },
-                        icon: const Icon(Icons.home)),
-                    index == 0
-                        ? const Text(
-                            "Home",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          )
-                        : const Text(""),
-                  ],
-                ),
-                Container(
-                  width: _size.width * 0.02,
-                ),
-                Column(
-                  mainAxisAlignment: index == 0
-                      ? MainAxisAlignment.center
-                      : MainAxisAlignment.start,
-                  children: [
-                    IconButton(
-                        color: Colors.white,
-                        onPressed: () {
-                          setState(() {
-                            index = 1;
-                            _onNavChange(index);
-                          });
-                        },
-                        icon: const Icon(Icons.menu)),
-                    index == 1
-                        ? const Text(
-                            "Menu",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          )
-                        : const Text(""),
-                  ],
-                ),
-              ],
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                mainAxisAlignment: index == 1
+                    ? MainAxisAlignment.center
+                    : MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                      color: Colors.white,
+                      onPressed: () {
+                        setState(() {
+                          index = 0;
+                          _onNavChange(index);
+                          archHeightController.reverse(from: 1.0);
+                        });
+                      },
+                      icon: const Icon(Icons.home)),
+                  index == 0
+                      ? const Text(
+                          "Home",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        )
+                      : const Text(""),
+                ],
+              ),
+              Container(
+                width: _size.width * 0.02,
+              ),
+              Column(
+                mainAxisAlignment: index == 0
+                    ? MainAxisAlignment.center
+                    : MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                      color: Colors.white,
+                      onPressed: () {
+                        setState(() {
+                          index = 1;
+                          _onNavChange(index);
+                          archHeightController.reverse(from: 1.0);
+                        });
+                      },
+                      icon: const Icon(Icons.menu)),
+                  index == 1
+                      ? const Text(
+                          "Menu",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        )
+                      : const Text(""),
+                ],
+              ),
+            ],
           ),
         ],
       ),
@@ -111,27 +196,38 @@ class NavBarCustomPainter extends CustomPainter {
     path.lineTo(size.width, 0);
   }
 
-  void drawAtLeft(Path path, Size size, Canvas canvas) {
-    path.lineTo(size.width * 0.10, 0);
-    path.quadraticBezierTo(size.width * 0.15, 0, size.width * 0.15, 30);
-    path.arcToPoint(Offset(size.width * 0.35, 30),
-        radius: const Radius.circular(20.0), clockwise: false);
-    path.quadraticBezierTo(size.width * 0.35, 0, size.width * 0.40, 0);
+  void drawAtLeft(Path path, Size size, Canvas canvas, double archHeight,
+      Map<String, Animation<double>?> tabPositions) {
+    path.lineTo(size.width * (tabPositions["a1"]?.value ?? 0), 0);
+    path.quadraticBezierTo(size.width * (tabPositions["a2"]?.value ?? 0), 0,
+        size.width * (tabPositions["a2"]?.value ?? 0), 30);
+    path.arcToPoint(Offset(size.width * (tabPositions["a3"]?.value ?? 0), 30),
+        radius: Radius.elliptical(20, archHeight), clockwise: false);
+    path.quadraticBezierTo(size.width * (tabPositions["a3"]?.value ?? 0), 0,
+        size.width * (tabPositions["a4"]?.value ?? 0), 0);
     path.lineTo(size.width, 0);
   }
 
-  void drawAtRight(Path path, Size size, Canvas canvas) {
-    path.lineTo(size.width * 0.61, 0);
-    path.quadraticBezierTo(size.width * 0.66, 0, size.width * 0.66, 30);
-    path.arcToPoint(Offset(size.width * 0.86, 30),
-        radius: const Radius.circular(20.0), clockwise: false);
-    path.quadraticBezierTo(size.width * 0.86, 0, size.width * 0.91, 0);
+  void drawAtRight(Path path, Size size, Canvas canvas, double archHeight,
+      Map<String, Animation<double>?> tabPositions) {
+    path.lineTo(size.width * (tabPositions["a1"]?.value ?? 0), 0);
+    path.quadraticBezierTo(size.width * (tabPositions["a2"]?.value ?? 0), 0,
+        size.width * (tabPositions["a2"]?.value ?? 0), 30);
+    path.arcToPoint(Offset(size.width * (tabPositions["a3"]?.value ?? 0), 30),
+        radius: Radius.elliptical(20, archHeight), clockwise: false);
+    path.quadraticBezierTo(size.width * (tabPositions["a3"]?.value ?? 0), 0,
+        size.width * (tabPositions["a4"]?.value ?? 0), 0);
     path.lineTo(size.width, 0);
   }
 
   int index;
+  double archRadius;
+  Map<String, Animation<double>?> tabPositions;
 
-  NavBarCustomPainter(this.index);
+  NavBarCustomPainter(
+      {required this.index,
+      required this.archRadius,
+      required this.tabPositions});
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -145,13 +241,13 @@ class NavBarCustomPainter extends CustomPainter {
 
     switch (index) {
       case 0:
-        drawAtLeft(path, size, canvas);
+        drawAtLeft(path, size, canvas, archRadius, tabPositions);
         break;
       case 1:
-        drawAtRight(path, size, canvas);
+        drawAtRight(path, size, canvas, archRadius, tabPositions);
         break;
       default:
-        drawAtLeft(path, size, canvas);
+        drawAtLeft(path, size, canvas, archRadius, tabPositions);
     }
 
     path.lineTo(size.width, size.height);
@@ -164,6 +260,6 @@ class NavBarCustomPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+    return true;
   }
 }
