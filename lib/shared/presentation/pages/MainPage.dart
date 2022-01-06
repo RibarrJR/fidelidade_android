6f4d2d45708e1a398d587a4fa3099ed2942c13e5
AdrianoValadaresFrwk <adrianovaladares@frwk.com.br>
2022-01-06 14:47:07 -0300

    Atualização da branch

--- lib/shared/presentation/pages/MainPage.dart
+++ lib/shared/presentation/pages/MainPage.dart
@@ -4,7 +4,6 @@ import 'package:fidelidade_android/utils/global.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import '../widgets/AppNavBar.dart';
-import 'package:fidelidade_android/shared/controller/MainPageController.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -15,17 +14,26 @@ class MainPage extends StatefulWidget {
   }
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
   // Para escopo local, gerar como na linha abaixo
   // MainPageController mainPageController = MainPageController();
 
   // Para escopo global, veja os arquivos /main.dart e utils/global.dart
 
+  late TabController _tabController;
   final screens = [const HomePage(), const MenuPage()];
 
   void onNavChanges(int index) {
     // ação do controller
     mainPageController.setScreenIndex(index);
+    _tabController.animateTo(mainPageController.screenIndex,
+        duration: const Duration(seconds: 1));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
   }
 
   @override
@@ -34,10 +42,17 @@ class _MainPageState extends State<MainPage> {
       // o widget Observer é necessário para o ter refresh da tela em alteração de estado
       body: Observer(
         builder: (context) {
-          return IndexedStack(
-            // o estado do controller
-            index: mainPageController.screenIndex,
-            children: screens,
+          // return IndexedStack(
+          //   // o estado do controller
+          //   index: mainPageController.screenIndex,
+          //   children: screens,
+          // );
+          return TabBarView(
+            controller: _tabController,
+            children: const <Widget>[
+              HomePage(),
+              MenuPage(),
+            ],
           );
         },
       ),
