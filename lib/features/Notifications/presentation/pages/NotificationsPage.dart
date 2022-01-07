94323f37d1ec2012c26a6d3d511476cf889ae4b5
raiffe.pontes <raiffepontes@frwk.com.br>
2022-01-07 15:22:26 -0300

    Messagem para quando não tiver notificações

--- lib/features/Notifications/presentation/pages/NotificationsPage.dart
+++ lib/features/Notifications/presentation/pages/NotificationsPage.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: unnecessary_const
 
 import 'package:fidelidade_android/shared/presentation/widgets/CustomAppBar.dart';
+import 'package:fidelidade_android/utils/Images.dart';
 import 'package:fidelidade_android/utils/constants.dart';
 import 'package:fidelidade_android/features/Notifications/presentation/widgets/NotificationCardWidget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class NotificationsPage extends StatefulWidget {
   const NotificationsPage({Key? key}) : super(key: key);
@@ -28,27 +30,50 @@ class _NotificationsPageState extends State<NotificationsPage> {
           title: "Notificações",
           automaticallyImplyLeading: true,
         ),
-        body: ListView.builder(
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              final item = items[index];
-              if (index == 0) {
-                return Column(
-                  children: [
-                    const NotificationDate(),
-                    NotificationCard(
+        body: items.isNotEmpty
+            ? ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  final item = items[index];
+                  if (index == 0) {
+                    return Column(
+                      children: [
+                        const NotificationDate(),
+                        NotificationCard(
+                            item: item,
+                            removeNotification: () =>
+                                removeNotification(items, index)),
+                      ],
+                    );
+                  }
+
+                  return NotificationCard(
                       item: item,
                       removeNotification: () =>
-                        removeNotification(items, index)),
-                  ],
-                );
-              }
-
-              return NotificationCard(
-                  item: item,
-                  removeNotification: () => removeNotification(items, index));
-            })
-        );
+                          removeNotification(items, index));
+                })
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                      child: SvgPicture.asset(
+                        fileEmpty,
+                        width: 259,
+                        height: 324,
+                  )),
+                  const SizedBox(height: 96),
+                  const Text(
+                    'Tudo certo, te avisaremos quando houver novas notificações!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(                        
+                        color: disabledBg,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold
+                      ),
+                  ),
+                ],
+              ));
   }
 }
 
