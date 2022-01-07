6ea0c5a9aa729ce94cf6c1d3239cdb69cf0ae209
AdrianoValadaresFrwk <95626253+AdrianoValadaresFrwk@users.noreply.github.com>
2022-01-07 15:06:44 -0300

    Feature/Animação da nav bar (#77)
    * Teste de animações
    
    * Atualização da branch
    
    * Implementação das animações da nav bar
    
    * Remoção do swipe da tabView

--- lib/shared/presentation/pages/MainPage.dart
+++ lib/shared/presentation/pages/MainPage.dart
@@ -1,10 +1,10 @@
 import 'package:fidelidade_android/features/Home/presentation/pages/HomePage.dart';
 import 'package:fidelidade_android/features/Menu/presentation/pages/MenuPage.dart';
 import 'package:fidelidade_android/utils/global.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import '../widgets/AppNavBar.dart';
-import 'package:fidelidade_android/shared/controller/MainPageController.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -15,17 +15,26 @@ class MainPage extends StatefulWidget {
   }
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
   // Para escopo local, gerar como na linha abaixo
   // MainPageController mainPageController = MainPageController();
 
   // Para escopo global, veja os arquivos /main.dart e utils/global.dart
 
+  late TabController _tabController;
   final screens = [const HomePage(), const MenuPage()];
 
   void onNavChanges(int index) {
     // ação do controller
     mainPageController.setScreenIndex(index);
+    _tabController.animateTo(mainPageController.screenIndex,
+        duration: const Duration(seconds: 1));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
   }
 
   @override
@@ -34,10 +43,18 @@ class _MainPageState extends State<MainPage> {
       // o widget Observer é necessário para o ter refresh da tela em alteração de estado
       body: Observer(
         builder: (context) {
-          return IndexedStack(
-            // o estado do controller
-            index: mainPageController.screenIndex,
-            children: screens,
+          // return IndexedStack(
+          //   // o estado do controller
+          //   index: mainPageController.screenIndex,
+          //   children: screens,
+          // );
+          return TabBarView(
+            controller: _tabController,
+            physics: const NeverScrollableScrollPhysics(),
+            children: const <Widget>[
+              HomePage(),
+              MenuPage(),
+            ],
           );
         },
       ),
