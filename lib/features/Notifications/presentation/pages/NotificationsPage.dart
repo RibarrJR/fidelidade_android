5aaf25f86868cae7228ce88b2cd7c2615896b2fb
raiffe.pontes <raiffepontes@frwk.com.br>
2022-01-11 11:23:01 -0300

    Listagem de notificações por data e ajustes

--- lib/features/Notifications/presentation/pages/NotificationsPage.dart
+++ lib/features/Notifications/presentation/pages/NotificationsPage.dart
@@ -6,6 +6,7 @@ import 'package:fidelidade_android/utils/constants.dart';
 import 'package:fidelidade_android/features/Notifications/presentation/widgets/NotificationCardWidget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:grouped_list/grouped_list.dart';
 
 class NotificationsPage extends StatefulWidget {
   const NotificationsPage({Key? key}) : super(key: key);
@@ -15,7 +16,15 @@ class NotificationsPage extends StatefulWidget {
 }
 
 class _NotificationsPageState extends State<NotificationsPage> {
-  final items = List<String>.generate(6, (i) => 'Item ${i + 1}');
+  final items = List<Object>.generate(
+    9,
+    (i) => {
+      'date': '1${i <= 2 ? 0 : i <= 5 ? 1 : 2}/01/2022',
+      'title': 'Você Ganhou!',
+      'message': '300 moedas',
+      'message2': ' Drogaria Araújo'
+    },
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -31,46 +40,44 @@ class _NotificationsPageState extends State<NotificationsPage> {
           automaticallyImplyLeading: true,
         ),
         body: items.isNotEmpty
-            ? ListView.builder(
-                itemCount: items.length,
-                itemBuilder: (context, index) {
-                  final item = items[index];
-                  if (index == 0) {
-                    return Column(
-                      children: [
-                        const NotificationDate(),
-                        NotificationCard(
-                            item: item,
-                            removeNotification: () =>
-                                removeNotification(items, index)),
-                      ],
-                    );
-                  }
-
+            ? GroupedListView<dynamic, String>(
+                elements: items,
+                floatingHeader: true,
+                useStickyGroupSeparators: true,
+                groupBy: (element) => element['date'],
+                groupComparator: (value1, value2) => value2.compareTo(value1),
+                itemComparator: (item1, item2) =>
+                    item1['message'].compareTo(item2['message']),
+                order: GroupedListOrder.ASC,
+                groupSeparatorBuilder: (String value) =>
+                    NotificationDate(date: value),
+                indexedItemBuilder: (c, item, index) {
                   return NotificationCard(
-                      item: item,
+                      title: item['title'],
+                      message: item['message'],
+                      message2: item['message2'],
                       removeNotification: () =>
                           removeNotification(items, index));
-                })
+                },
+              )
             : Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Center(
                       child: SvgPicture.asset(
-                        fileEmpty,
-                        width: 259,
-                        height: 324,
+                    fileEmpty,
+                    width: 259,
+                    height: 324,
                   )),
                   const SizedBox(height: 96),
                   const Text(
                     'Tudo certo, te avisaremos quando houver novas notificações!',
                     textAlign: TextAlign.center,
-                    style: TextStyle(                        
+                    style: TextStyle(
                         color: disabledBg,
                         fontSize: 24,
-                        fontWeight: FontWeight.bold
-                      ),
+                        fontWeight: FontWeight.bold),
                   ),
                 ],
               ));
@@ -78,37 +85,40 @@ class _NotificationsPageState extends State<NotificationsPage> {
 }
 
 class NotificationDate extends StatelessWidget {
-  const NotificationDate({
-    Key? key,
-  }) : super(key: key);
+  final String date;
+
+  const NotificationDate({Key? key, required this.date}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Padding(
-          padding: EdgeInsets.only(left: 13.0, top: 45),
-          child: Text(
-            "28/12/2021",
-            style: TextStyle(
-                fontWeight: FontWeight.normal, color: primaryColor, fontSize: 18),
+    return Container(
+      height: 76.0,
+      width: MediaQuery.of(context).size.width,
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 13.0, top: 25),
+            child: Text(
+              date,
+              style: const TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: primaryColor,
+                  fontSize: 18),
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(
-            top: 8, 
-            bottom: 21,
-            left: 16,
-            right: 16        
+          Padding(
+            padding:
+                const EdgeInsets.only(top: 8, bottom: 21, left: 16, right: 16),
+            child: Container(
+              color: primaryColor,
+              height: 1,
+            ),
           ),
-          child: Container(
-            color: primaryColor,
-            height: 1,
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
